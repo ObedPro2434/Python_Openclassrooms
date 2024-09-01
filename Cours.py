11f2fe804a171bbc1ffa2aacdf1c24eaa4b3dabd
Obed KPOGO <kpogok2434@yahoo.com>
2024-09-01 08:02:29 +0100

    Prise de note de cours et exercices

---
+++ Cours.py
@@ -0,0 +1,9 @@
+# Utilisation de la fonction f-string
+nom = "Noëlie"
+prenom = "Obed"
+age = 28
+
+print(f"Bonjour, je m'appelle {prenom} {nom} et j'ai {age} ans.")
+
+#Une autre manière de proceder , mais cette methode est plus compliqué
+print("Bonjour, je m'appelle " + prenom + " " + nom + " et j'ai " + str(age) + "ans")
--- main.py
+++ main.py
@@ -1,2 +1,5 @@
-print("Japprends Python")
+# Ecrire le message avec la fonction print
+print("J'apprends Python")
+
+#Afficher le resultat d'un calcul
 print("17 + 35 * 2 = ",17 + 35 * 2)
