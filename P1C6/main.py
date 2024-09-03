4d9fc45489efdac00517b90e43fc361f6432c843
ObedInfor <kpogok2434@yahoo.com>
2024-09-03 20:15:03 +0100

    P1C6

---
+++ P1C6/main.py
@@ -0,0 +1,18 @@
+# Création d'une liste de fruits
+fruits = ["pomme", "banane", "orange"]
+print(fruits)
+
+# Ajout d'autres fruits 
+fruits.append("kiwi")
+print(fruits)
+
+# Suppression de orange de la liste fruits
+fruits.remove("orange")
+print(fruits)
+
+#longueur de la liste fruits
+print(len(fruits))
+
+#triage par odre alphabetique
+fruits.sort()
+print(fruits)
