4418ae33e9f69068b06a5bfdb47603e15b0a07a6
ObedInfor <kpogok2434@yahoo.com>
2024-09-05 06:53:04 +0100

    Changement de dossier

--- Dictionnaire.py
+++
@@ -1,6 +0,0 @@
-taux_conver = {}
-taux_conver["fcbk"] = 4.2
-taux_conver["istg"] = 3.14
-
-nouvelle_campagne = {"responsable_de_campagne": "Jean-Jacques", "nom_de_compagne": "Campagne nous aimons les chiens", "date_de_debut": "01/10/2024", "influenceurs_importants": ["OlivierGiroux", "FredéricPotcho"]}
-nouvelle_campagne["responsable_de_campagne"]
